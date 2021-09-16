e9446f09e890948166cf76bdda728f7b55b3f937
codybravo <armjacob1@gmail.com>
2021-09-16 20:21:23 +0530

    unpredictable behaviour of contenteditable some time tooltip working some time not

--- content.js
+++ content.js
@@ -1,24 +1,10 @@
 const less = document.getElementsByClassName("m9osqain a5q79mjw gy2v8mqq jm1wdb64 k4urcfbm qv66sw1b");
 const bess = document.getElementsByClassName("q5bimw55 rpm2j7zs k7i0oixp gvuykj2m j83agx80 cbu4d94t ni8dbmo4 eg9m0zos l9j0dhe7 du4w35lb ofs802cu pohlnb88 dkue75c7 mb9wzai9 l56l04vs r57mb794 kh7kg01d c3g1iek1 buofh1pr");
 
-
+const contEdit = document.getElementsByClassName("rq0escxv buofh1pr df2bnetk hv4rvrfc dati1w0a l9j0dhe7 k4urcfbm du4w35lb gbhij3x4")
 const cess = document.getElementsByClassName("rq0escxv buofh1pr df2bnetk hv4rvrfc dati1w0a l9j0dhe7 k4urcfbm du4w35lb gbhij3x4")
 const  tess = document.getElementById("test-tooltip");
 
-// const less = document.getElementsByClassName("ihqw7lf3");
-// const less = document.getElementsByClassName("rz4wbd8a");
-// const less = document.getElementsByClassName("discj3wi");
-// const less = document.getElementsByClassName("dhix69tm");
-// const less = document.getElementsByClassName("wkznzc2l");
-
-// let scrpt =
-// `
-// <script>
-// function myConsole() {
-//   console.log("hello man world")
-// }
-// </script>
-// ` 
 let html =
 `
 <br>
@@ -26,11 +12,19 @@ let html =
   <span id="test-tooltip" class="tooltiptext"> 
     <button onClick="(function(){
       document.execCommand('bold',false,null);
-      alert('Hey i am calling'); 
+      alert(window.getSelection().getRangeAt(0));
       return false;
   })();return false;" type="button" id="bold">B</button>
-    <button type="button" id="italic" >I</button>
-    <button type="button" id="underline">U</button>
+    <button onClick="(function(){
+      document.execCommand('italic',false,null);
+      alert(window.getSelection().);
+      return false;
+  })();return false;" type="button" id="italic" >I</button>
+    <button onClick="(function(){
+      document.execCommand('underline',false,null);
+      alert(window.getSelection());
+      return false;
+  })();return false;" type="button" id="underline">U</button>
     <button type="button" id="h1">H!</button>
     <button type="button" id="h2">H2</button>
     <button type="button" id="quote">"</button>
@@ -41,9 +35,6 @@ let html =
 <br>
 
 `;
-
-  
-
 
 // injecting notification block
 
@@ -56,46 +47,27 @@ if(less){
       console.log("clicked");
       setTimeout(() => {
           if(bess)
-        {
-          console.log("Exists!");
-          for(let j=0; j < bess.length; j++)
           {
-            // bess[j].insertAdjacentHTML("afterend", scrpt);
-            bess[j].insertAdjacentHTML("afterend", html);
-             
-          } 
-        }
+            console.log("Exists!");
+            for(let j=0; j < bess.length; j++)
+            {
+              bess[j].insertAdjacentHTML("afterend", html);  
+            } 
+          }
+          if(contEdit)
+          {
+            console.log(contEdit.length,"Exists constEdit!");
+            for(let j=0; j < contEdit.length; j++)
+            {
+              contEdit[j].contentEditable = "true"  
+            }
+          }
       },2000)
     })
   }
   
 }
-// function myTimer() {
-//   var bold = document.getElementById('bold');
-//   if(bold){
-//     document.getElementById('bold').addEventListener("click", () => {
-//       console.log("done");
-//       clearInterval(myVar);
-//       // document.execCommand('bold',false,null);
-//     })
-//   }else{
-//     console.log("no button discovered")
-//   }
-// }
-// const myVar = setInterval(myTimer,3000);
 
 
 
-// if(bold){
-//   bold.addEventListener("click", () => {
-//     console.log("Doing bold");
-//     document.execCommand('bold',false,null);
-//   })
-// }
-
-
-
-// if(less) {
-// less.insertAdjacentHTML("afterbegin", html);
-// }
 
